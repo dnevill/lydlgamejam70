294da1e30ff524264d6f32f5a31cf2d2a0f47662
dnevill <dnevill@gmail.com>
2024-06-17 13:16:55 -0600

    Assets Import and player updates
    Moved player into their own tscn for ease of sublevel testing. Imported assets provided so far by artists, applied animations and logic to player.

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -15,18 +15,21 @@ func _ready():
 func _process(delta):
 	
 	if Input.is_action_pressed("ui_left"):
-		$AnimatedSprite2D.flip_h = true;
+		$PlayerAnimSprite.flip_h = true;
 		if linear_velocity.x > -MAXSPEED:
 			apply_central_impulse(Vector2(-HORZACCEL,0))
 	elif Input.is_action_pressed("ui_right"):
-		$AnimatedSprite2D.flip_h = false;
+		$PlayerAnimSprite.flip_h = false;
 		scale = Vector2(1,1)
 		if linear_velocity.x < MAXSPEED:
 			apply_central_impulse(Vector2(HORZACCEL,0))
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		#start some jump animation
+		$PlayerAnimSprite.play("jump")
 		print("You jumped! and " + str(is_on_floor()))
 		apply_central_impulse(Vector2(0,-VERTACCEL))
+	elif is_on_floor():
+		$PlayerAnimSprite.play("walk")
 
 func is_on_floor():
 	if self.test_move(transform, Vector2.DOWN):
