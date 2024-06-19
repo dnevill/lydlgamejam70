5e061ba8899cbe17c09f28f766fa008d0a57a873
GreenCastleB <secondepoch@gmail.com>
2024-06-19 11:26:55 -0500

    Creating a Sub-Level
    Started Scene1-2.tscn.
    Wrote code that includes it within Node_World at a hard-coded position.

--- Scripts/Node_World.gd
+++ Scripts/Node_World.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+@onready var OtherSceneNode = $OtherScenes;
+
 const TILE_SIZE = 32;
 
 func _on_World_ready():
+	#
+	# Includes Scene 1-2 after 30 tiles of main level.
+	#
+	var newScene = load("res://Scenes/Scene1-2.tscn");
+	var newSceneInst = newScene.instantiate();
+	OtherSceneNode.add_child(newSceneInst);
+	newSceneInst.position.x = TILE_SIZE * 30;
+	#
 	pass;
