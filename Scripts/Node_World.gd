1a5dc73e839c542e4b04edb0c326cd5de66503c9
GreenCastleB <secondepoch@gmail.com>
2024-06-19 19:31:20 -0500

    Scene 3-2
    Includes tileset update.

--- Scripts/Node_World.gd
+++ Scripts/Node_World.gd
@@ -6,9 +6,9 @@ const TILE_SIZE = 32;
 
 func _on_World_ready():
 	#
-	# Includes Scene 1-2 after 30 tiles of main level.
+	# Includes a Scene after 30 tiles of main level.
 	#
-	var newScene = load("res://Scenes/Scene1-2.tscn");
+	var newScene = load("res://Scenes/Scene3-2.tscn");
 	var newSceneInst = newScene.instantiate();
 	OtherSceneNode.add_child(newSceneInst);
 	newSceneInst.position.x = TILE_SIZE * 30;
