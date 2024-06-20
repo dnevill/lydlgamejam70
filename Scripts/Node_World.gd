877bedb63416db6a3d9b0a0fbc9d7a80363fb43d
GreenCastleB <secondepoch@gmail.com>
2024-06-19 22:04:15 -0500

    Scene 2-2

--- Scripts/Node_World.gd
+++ Scripts/Node_World.gd
@@ -8,7 +8,7 @@ func _on_World_ready():
 	#
 	# Includes a Scene after 30 tiles of main level.
 	#
-	var newScene = load("res://Scenes/Scene3-2.tscn");
+	var newScene = load("res://Scenes/Scene2-2.tscn");
 	var newSceneInst = newScene.instantiate();
 	OtherSceneNode.add_child(newSceneInst);
 	newSceneInst.position.x = TILE_SIZE * 30;
