7d34ab04bf85cf4f7e849e92c64e5fd4d51fa684
dnevill <dnevill@gmail.com>
2024-06-21 13:41:01 -0600

    Fisherman first pass and fish

---
+++ Assets/NPCs/fish.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer2.play("wiggle")
+	
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Assets/NPCs/fisherman.gd
@@ -0,0 +1,71 @@
+extends Node2D
+@onready var dialogBox : DialogBox = $Dialog
+
+var ready_to_interact = true;
+var waiting_to_interact = false;
+var disabledPlayer = null
+
+#$enum DeerFates {DIDNT_MEET_YET, HELPED, SCARED, IGNORED}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	dialogBox.SetDialogText("UwU I'm just a fishin' pal fishin' away at thes fishes :3")
+	dialogBox.SetOption1Text("Ask for a fish")
+	dialogBox.SetOption2Text("Knock over the bucket of fish")
+	dialogBox.SetOption3Text("Knock over the fisherman")
+	dialogBox.SetPhoto($Sprite2D.texture)
+	#Probably need some code here to position the dialog box on screen at a good spot.
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "PlayerRigidBody" and ready_to_interact:
+		ready_to_interact = false
+		print("Hey the player touched me OMGEEEEE lets spawn a dialogbox")
+		body.in_dialog = true
+		disabledPlayer = body
+		dialogBox.Reveal()
+		$DialogSound.play()
+
+func _on_area_2d_body_exited(body):
+	if body.name == "PlayerRigidBody" and waiting_to_interact:
+		print("Hey the player stopped touchin' me")
+		waiting_to_interact = false
+		ready_to_interact = true
+
+
+
+
+func _on_dialog_option_selected(optionNumber):
+	disabledPlayer.in_dialog = false
+	match optionNumber:
+		1:
+			print("Asked for a feesh")
+			PlayerStateManager.FishermanFate = PlayerStateManager.FishermanFates.ASKED_FOR_FISH
+			ready_to_interact = false
+		2:
+			print("Knocked bucket")
+			PlayerStateManager.FishermanFate = PlayerStateManager.FishermanFates.BUCKET_KNOCKED
+			ready_to_interact = false
+		3:
+			print("Knocked fisherman")
+			PlayerStateManager.FishermanFate = PlayerStateManager.FishermanFates.FISHERMAN_KNOCKED
+			ready_to_interact = false
+	dialogBox.visible = false
+	
+
+
+func ShakeSprite():
+	pass
+
+func _on_timer_timeout():
+	if ready_to_interact:
+		$IdleSound.play()
+		$AnimationPlayer.play("shake quarter sec")
+
+
+func _on_idle_sound_finished():
+	$IdleSound/Timer.start()
