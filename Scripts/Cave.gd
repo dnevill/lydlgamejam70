c19fc10572f73b70f58582b45a6126fac45a2ac7
GreenCastleB <secondepoch@gmail.com>
2024-06-22 17:56:28 -0500

    Developing Cave Script
    Detects when player has rested within doorway for 90 frames.

--- Scripts/Cave.gd
+++ Scripts/Cave.gd
@@ -2,23 +2,49 @@ extends Node2D
 
 @onready var NudgeNode = $NudgeNode;
 
+const DOOR_RESTFRAMES = 90;
+
+var PlayerInDoor = false;
+var InDoorResting = DOOR_RESTFRAMES;
+
 func _ready():
 	NudgeNode.visible = false;
 	pass
 
 func _process(_delta):
-	pass
+	if(PlayerInDoor):
+		if(InDoorResting > 0):
+			if(InDoorResting == DOOR_RESTFRAMES):
+				NudgeNode.visible = false;
+			InDoorResting -= 1;
+			if(InDoorResting == 0):
+				print("EXIT CYCLE HERE");
 
 func _on_warning_area_body_entered(body):
 	if body.name == "PlayerRigidBody":
 		NudgeNode.get_node("NudgeAnim").play("NudgeIconBounce");
 		NudgeNode.visible = true;
-		pass
-	pass
+		pass;
+	pass;
 
 func _on_warning_area_body_exited(body):
 	if body.name == "PlayerRigidBody":
 		NudgeNode.visible = false;
 		NudgeNode.get_node("NudgeAnim").stop();
-		pass
-	pass
+		pass;
+	pass;
+
+
+func _on_doorway_area_body_entered(body):
+	if body.name == "PlayerRigidBody":
+		PlayerInDoor = true;
+		InDoorResting = DOOR_RESTFRAMES;
+	pass;
+
+
+func _on_doorway_area_body_exited(body):
+	if body.name == "PlayerRigidBody":
+		PlayerInDoor = false;
+		InDoorResting = DOOR_RESTFRAMES;
+		NudgeNode.visible = true;
+	pass;
