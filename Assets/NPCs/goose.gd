e67de55b27a41e2aff2c34774cde49fd6d016f6f
dnevill <dnevill@gmail.com>
2024-06-22 16:00:00 -0600

    Wing fox, goose charge, game scaling

--- Assets/NPCs/goose.gd
+++ Assets/NPCs/goose.gd
@@ -6,6 +6,7 @@ const LAUNCH_VECTOR = Vector2(1200,-1000)
 var ready_to_interact = true;
 var waiting_to_interact = false;
 var disabledPlayer : Player = null
+var ready_to_launch_again = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,7 +24,9 @@ func _on_area_2d_body_entered(body):
 	dialogBox.SetOption2Text("Ask the goose to carry you")
 	dialogBox.SetOption3Text("Ignore the goose")
 	dialogBox.SetPhoto($Sprite2D.texture)
-	if body.name == "PlayerRigidBody" and ready_to_interact:
+	if body.name == "PlayerRigidBody" and ready_to_launch_again:
+		body.apply_central_impulse(LAUNCH_VECTOR)
+	elif body.name == "PlayerRigidBody" and ready_to_interact:
 		ready_to_interact = false
 		print("Hey the player touched me OMGEEEEE lets spawn a dialogbox")
 		body.in_dialog = true
@@ -36,15 +39,8 @@ func _on_area_2d_body_exited(body):
 		print("Hey the player stopped touchin' me")
 		waiting_to_interact = false
 		ready_to_interact = true
-	elif body.name == "PlayerRigidBody" and PlayerStateManager.GooseFate == PlayerStateManager.GooseFates.HELPED:
-		body.apply_central_impulse(LAUNCH_VECTOR)
-		
-
-
-
 
 func _on_dialog_option_selected(optionNumber):
-	disabledPlayer.in_dialog = false
 	match optionNumber:
 		1:
 			print("Oh no the goose is dead")
@@ -61,21 +57,33 @@ func _on_dialog_option_selected(optionNumber):
 			tween.parallel().tween_property($Sprite2D, "rotation", 30, 4)
 			tween.tween_callback(queue_free)
 			ready_to_interact = false
+			disabledPlayer.in_dialog = false
 		2:
 			print("Hooray the goose carries the player here")
 			PlayerStateManager.GooseFate = PlayerStateManager.GooseFates.HELPED
-			disabledPlayer.apply_central_impulse(LAUNCH_VECTOR)
+			var tween = get_tree().create_tween()
+			$ChargeSFX.play()
+			tween.tween_property($".", "position", Vector2.LEFT * 128, 1.5).as_relative().from_current()
+			tween.parallel().tween_property($".", "scale", scale * Vector2(-1,1), 1.5)
+			tween.tween_property($".", "position", Vector2.RIGHT * 128, 0.07).as_relative().set_delay(1.5)
+			tween.tween_callback(player_launchtime)
 			ready_to_interact = false
 		3:
 			print("Ignored, set a timer to allow goose interaction again in a few seconds")
 			#Note that since it stays active, this fate might get set, then later adjusted back while they're still visible on camera
 			#So don't later on hook up an event listener to this fate to affect things with its on_changed or anything
 			PlayerStateManager.GooseFate = PlayerStateManager.GooseFates.IGNORED
+			disabledPlayer.in_dialog = false
 			ready_to_interact = false
 			waiting_to_interact = true
 	dialogBox.visible = false
 	
 
+func player_launchtime():
+	disabledPlayer.in_dialog = false
+	disabledPlayer.freeze = false
+	disabledPlayer.apply_central_impulse(LAUNCH_VECTOR)
+	ready_to_launch_again = true
 
 func ShakeSprite():
 	pass
@@ -88,3 +96,7 @@ func _on_timer_timeout():
 
 func _on_idle_sound_finished():
 	$IdleSound/Timer.start()
+
+
+func _on_charge_sfx_finished():
+	pass # Replace with function body.
