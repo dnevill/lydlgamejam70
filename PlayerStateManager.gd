ef446575596ef0639ea4b83f8330aa07dc58db68
dnevill <dnevill@gmail.com>
2024-06-22 11:54:09 -0600

    Node World loads sublevels, Home forces cycle finish
    Home is just a for debuggin' for us fwends

--- PlayerStateManager.gd
+++ PlayerStateManager.gd
@@ -10,6 +10,8 @@ enum HunterFates {DIDNT_MEET_YET, SOMETHING, SOMETHINGELSE}
 enum FieldNPCFates {DIDNT_MEET_YET, SOMETHING, SOMETHINGELESE}
 enum FarmerFates {DIDNT_MEET_YET, SNEAKED_IN, DID_SOMETHING_CUTE}
 
+var cycleNum = 1
+
 var GooseFate = GooseFates.DIDNT_MEET_YET
 var LumberjackAxeFate = LumberjackAxeFates.DIDNT_MEET_YET
 var FishermanFate = FishermanFates.DIDNT_MEET_YET
@@ -21,7 +23,16 @@ var FieldNPCFate = FieldNPCFates.DIDNT_MEET_YET
 var FarmerFate = FarmerFates.DIDNT_MEET_YET
 
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_home"):
+		PlayerStateManager.cycleNum += 1
+		if PlayerStateManager.cycleNum > 3:
+			resetfates()
+		get_tree().reload_current_scene()
+
+
 func printfates():
+	print("Cyclenumber: " + str(cycleNum))
 	print("GooseFate: " + GooseFates.find_key(GooseFate))
 	print("LumberjackAxeFate: " + LumberjackAxeFates.find_key(LumberjackAxeFate))
 	print("FishermanFate: " + FishermanFates.find_key(FishermanFate))
@@ -31,3 +42,15 @@ func printfates():
 	print("HunterFate: " + HunterFates.find_key(HunterFate))
 	print("FieldNPCFate: " + FieldNPCFates.find_key(FieldNPCFate))
 	print("FarmerFate: " + FarmerFates.find_key(FarmerFate))
+
+func resetfates():
+	cycleNum = 1
+	GooseFate = GooseFates.DIDNT_MEET_YET
+	LumberjackAxeFate = LumberjackAxeFates.DIDNT_MEET_YET
+	FishermanFate = FishermanFates.DIDNT_MEET_YET
+	DeerFate = DeerFates.DIDNT_MEET_YET
+	OxFate = OxFates.DIDNT_MEET_YET
+	FishermanIIFate = FishermanIIFates.DIDNT_MEET_YET
+	HunterFate = HunterFates.DIDNT_MEET_YET
+	FieldNPCFate = FieldNPCFates.DIDNT_MEET_YET
+	FarmerFate = FarmerFates.DIDNT_MEET_YET
--- Scripts/Node_World.gd
+++ Scripts/Node_World.gd
@@ -12,16 +12,52 @@ const SUBLEVEL_LENGTH = 73; # tile count in a subscene, there are more clever wa
 #not worth doing all this for a jammy jam though
 
 var rootX = self.position.x;
+const W1_1 = preload("res://Scenes/Scene1-1pond.tscn")
+const W1_2 = preload("res://Scenes/Scene1-2forest.tscn")
+
+const W2_1 = preload("res://Scenes/Scene2-1pond.tscn")
+const W2_2forest = preload("res://Scenes/Scene2-2forest.tscn")
+const W2_2field = preload("res://Scenes/Scene2-2forest.tscn")
+
+const W3_1fish = preload("res://Scenes/Scene3-1fish.tscn")
+const W3_1frog = preload("res://Scenes/Scene3-1frog.tscn")
+
+const W3_2farm = preload("res://Scenes/Scene3-2farm.tscn")
+const W3_2field = preload("res://Scenes/Scene3-2field.tscn")
+const W3_2forest = preload("res://Scenes/Scene3-2forest.tscn")
+
 
 func _on_World_ready():
+	var newScene1 = W1_1
+	var newScene2 = W1_2
+	PlayerStateManager.printfates()
+	match PlayerStateManager.cycleNum:
+		2:
+			newScene1 = W2_1
+			if PlayerStateManager.LumberjackAxeFate != PlayerStateManager.LumberjackAxeFates.GAVE:
+				newScene2 = W2_2forest
+			else:
+				newScene2 = W2_2field
+		3:
+			if PlayerStateManager.FishermanFate == PlayerStateManager.FishermanFates.FISHERMAN_KNOCKED or PlayerStateManager.FishermanFate == PlayerStateManager.FishermanFates.BUCKET_KNOCKED:
+				newScene1 = W3_1fish
+			else:
+				newScene2 = W3_1frog
+			
+			if PlayerStateManager.LumberjackAxeFate != PlayerStateManager.LumberjackAxeFates.GAVE:
+				newScene2 = W3_2forest
+			elif PlayerStateManager.OxFate == PlayerStateManager.OxFates.RELEASED:
+				newScene2 = W3_2field
+			else:
+				newScene2 = W3_2farm
 	# insert 1st sublevel
-	var newScene1 = load("res://Scenes/Scene3-1fish.tscn");
+
 	var newSceneInst1 = newScene1.instantiate();
 	OtherSceneNode.add_child(newSceneInst1);
 	newSceneInst1.position.x = rootX + (TILE_SIZE * OPENING_LENGTH);
 	
 	# insert 2nd sublevel
-	var newScene2 = load("res://Scenes/Scene3-2forest.tscn");
+
 	var newSceneInst2 = newScene2.instantiate();
 	OtherSceneNode.add_child(newSceneInst2);
 	newSceneInst2.position.x = newSceneInst1.position.x + (TILE_SIZE * (SUBLEVEL_LENGTH + INBETWEEN_LENGTH));
