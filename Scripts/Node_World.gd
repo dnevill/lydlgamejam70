ab668f8b45193328a584aba9586f4ae47072b418
GreenCastleB <secondepoch@gmail.com>
2024-06-21 21:27:50 -0500

    Update Node_World.gd
    Code to add Cave scene at end.

--- Scripts/Node_World.gd
+++ Scripts/Node_World.gd
@@ -3,22 +3,33 @@ extends Node2D
 @onready var OtherSceneNode = $OtherScenes;
 
 const TILE_SIZE = 32;
-const SUBLEVEL_LENGTH = 73 #tile count in a subscene, there are more clever ways to do this
+
+const OPENING_LENGTH = 45; # where, in tiles, should the first sublevel be inserted
+const INBETWEEN_LENGTH = 0; # how many tiles are between the first and second sublevel
+const SUBLEVEL_LENGTH = 73; # tile count in a subscene, there are more clever ways to do this
 #a more polished version of this would ask each sublevel for the right coordinates to put the next sublevel
 #also probably specify the y offset of the 'ground' in each since it could vary in theory
 #not worth doing all this for a jammy jam though
 
-func _on_World_ready():
-	#
-	# Includes a scene after 30 tiles of main level, and another scene at the sublevel length in tiles beyond that
-	#
+var rootX = self.position.x;
 
-	var newScene = load("res://Scenes/Scene1-1pond.tscn");
-	var newSceneInst = newScene.instantiate();
-	OtherSceneNode.add_child(newSceneInst);
-	newSceneInst.position.x = TILE_SIZE * 30;
-	var newScene2 = load("res://Scenes/Scene1-2forest.tscn");
+func _on_World_ready():
+	# insert 1st sublevel
+	var newScene1 = load("res://Scenes/Scene3-1fish.tscn");
+	var newSceneInst1 = newScene1.instantiate();
+	OtherSceneNode.add_child(newSceneInst1);
+	newSceneInst1.position.x = rootX + (TILE_SIZE * OPENING_LENGTH);
+	
+	# insert 2nd sublevel
+	var newScene2 = load("res://Scenes/Scene3-2forest.tscn");
 	var newSceneInst2 = newScene2.instantiate();
 	OtherSceneNode.add_child(newSceneInst2);
-	newSceneInst2.position.x = TILE_SIZE * SUBLEVEL_LENGTH + newSceneInst.position.x;
-
+	newSceneInst2.position.x = newSceneInst1.position.x + (TILE_SIZE * (SUBLEVEL_LENGTH + INBETWEEN_LENGTH));
+	
+	# insert cave
+	var newCave = load("res://Scenes/Cave.tscn");
+	var newCaveInst = newCave.instantiate();
+	OtherSceneNode.add_child(newCaveInst);
+	newCaveInst.position.x = newSceneInst2.position.x + (TILE_SIZE * SUBLEVEL_LENGTH);
+	
+	pass;
