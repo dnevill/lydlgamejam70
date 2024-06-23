fae0e5d2a7789a27a099e45f09acd8125c6a6445
GreenCastleB <secondepoch@gmail.com>
2024-06-23 00:47:04 -0500

    Developed Credits Scene
    Credits Roll animation
    Credits Sprites
    Back Button click / ui_accept

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -1,16 +1,21 @@
 extends Control
 
-@onready var StageNode = $StageNode;
+@onready var StageAnim = $StageAnim;
 @onready var Parallax = $ParallaxBackground;
 var mainMenuSCN = preload("res://Scenes/main_menu.tscn");
 
 func _ready():
 	$ParallaxBackground.scroll_offset = Vector2(PlayerStateManager.creditsPBGoffset,0);
+	$StageAnim.play("CreditsRoll");
 
-func _process(_delta):
-	Parallax.scroll_offset += Vector2(8,0);
-	StageNode.position.y -= 4;
-
-func _on_back_pressed():
+func go_back():
 	PlayerStateManager.creditsPBGoffset = $ParallaxBackground.scroll_offset.x;
 	get_tree().change_scene_to_packed(mainMenuSCN);
+
+func _process(_delta):
+	Parallax.scroll_offset += Vector2(-1,0);
+	if Input.is_action_just_pressed("ui_accept"):
+		go_back();
+
+func _on_back_pressed():
+	go_back();
